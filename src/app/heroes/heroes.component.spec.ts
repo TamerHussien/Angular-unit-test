2dfafcef0a4387cb5979c5bb9da9f18cf84a671c
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2019-04-14 08:48:44 +0200

    M-34-35: Component Test, Mocking to isolate Code

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,28 @@
+import { HeroesComponent } from './heroes.component';
+import { of } from 'rxjs';
+
+describe(`description`, () => {
+    let component: HeroesComponent;
+    let HEROES;
+    let mockHeroService;
+    beforeEach(() => {
+        HEROES = [
+            {id: 1, name: 'SpiderDude', strength: 8},
+            {id: 1, name: 'Wonderful Woman', strength: 24},
+            {id: 1, name: 'BatMan', strength: 15}
+        ];
+        mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+        component = new HeroesComponent(mockHeroService);
+    });
+    describe('delete', () => {
+        it(`should remove the indicated hero from the heroes list`, () => {
+                // Arrange
+                mockHeroService.deleteHero.and.returnValue(of(true));
+                component.heroes = HEROES;
+                // Act
+                component.delete(HEROES[2]);
+                // Assert
+                expect(component.heroes.length).toBe(2);
+        });
+    });
+});
