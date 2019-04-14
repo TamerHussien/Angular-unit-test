67675efe860ab7366a16083f8d16aa19b2c97827
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2019-04-14 20:34:50 +0200

    Fix: Pipe Tests

--- src/app/strength/strength.pipe.spec.ts
+++ src/app/strength/strength.pipe.spec.ts
@@ -7,13 +7,13 @@ describe('Strength Pipe', () => {
         expect(pipe.transform(5)).toEqual('5 (weak)');
     });
 
-    it('Should display weak if strength is 5', () => {
+    it('Should display strong if strength is 10', () => {
         const pipe = new StrengthPipe();
 
         expect(pipe.transform(10)).toEqual('10 (strong)');
     });
 
-    it('Should display weak if strength is 5', () => {
+    it('Should display unbelievable if strength is 20', () => {
         const pipe = new StrengthPipe();
 
         expect(pipe.transform(20)).toEqual('20 (unbelievable)');
