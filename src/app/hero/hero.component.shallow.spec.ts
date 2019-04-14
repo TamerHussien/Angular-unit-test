f934dc96e0f366f0815a4f566e820e1c7343a927
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2019-04-14 15:18:31 +0200

    M-4-3&4: Adding TestBed and NO_ERRORS_SCHEMA

---
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { HeroComponent } from './hero.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+describe(`HeroComponent (Shallow Tests)`, () => {
+let fixture: ComponentFixture<HeroComponent>;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [HeroComponent],
+            schemas: [NO_ERRORS_SCHEMA],
+            });
+
+            fixture = TestBed.createComponent(HeroComponent);
+    });
+    it(`should have the correct hero`, () => {
+        // Arrange
+        fixture.componentInstance.hero = {id: 1, name: 'SuperDude', strength: 3};
+        // Act
+
+        // Assert
+        expect(fixture.componentInstance.hero.name).toEqual('SuperDude');
+    });
+});
