21b454d6c63c91494ad4b2463761a715a17a4c5c
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2019-04-14 15:47:00 +0200

    M-3-5&6: Testing HTML, Native Elem Vs. Debug Elem

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { HeroComponent } from './hero.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, ElementRef } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe(`HeroComponent (Shallow Tests)`, () => {
 let fixture: ComponentFixture<HeroComponent>;
@@ -19,5 +20,18 @@ let fixture: ComponentFixture<HeroComponent>;
 
         // Assert
         expect(fixture.componentInstance.hero.name).toEqual('SuperDude');
+    });
+
+    it(`should render the hero name in an anchor tag`, () => {
+            // Arrange
+            fixture.componentInstance.hero = {id: 1, name: 'SuperDude', strength: 3};
+
+            // Act
+            fixture.detectChanges();
+            // Assert
+            const deA = fixture.debugElement.query(By.css('a'));
+            expect(deA.nativeElement.textContent).toContain('SuperDude');
+
+            expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude');
     });
 });
